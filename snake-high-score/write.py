a8cd64b5dfe4bdfd6651cad1293e853c9100562f
Luccas Kunzler <luccasmk@gmail.com>
2021-12-07 15:32:58 +0100

    Inclusion of external file to keep high score

--- snake-high-score/write.py
+++ snake-high-score/write.py
@@ -7,13 +7,16 @@ class Write(Turtle):
         self.hideturtle()
         self.penup()
         self.score = 0
-        self.high_score = 0
+        with open('high_score.txt', "r") as file:
+            self.high_score = int(file.read())
         self.color('white')
         self.update_score()
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('high_score.txt', "w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_score()
 
